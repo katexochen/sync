8971e7d428f55f91281fdfc14e5ccad4fd5f3953
Paul Meyer <49727155+katexochen@users.noreply.github.com>
2024-02-23 18:17:28 +0100

    e2e: add concurrent fifo e2e
    Signed-off-by: Paul Meyer <49727155+katexochen@users.noreply.github.com>

--- client/fifo_e2e_test.go
+++ client/fifo_e2e_test.go
@@ -3,10 +3,14 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"math/rand"
 	"os"
+	"sync"
 	"testing"
+	"time"
 
 	"github.com/katexochen/sync/api"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -55,6 +59,70 @@ func TestFifoBasics(t *testing.T) {
 			ticketID: ticket,
 		}))
 	})
+}
+
+func TestFifoConcurrent100(t *testing.T) {
+	assert := assert.New(t)
+	require := require.New(t)
+	ctx := context.Background()
+	endpoint := endpoint()
+
+	var mux sync.Mutex
+	assertResourceExclusive := func() {
+		if ok := mux.TryLock(); !ok {
+			require.Fail("tried to access resource in use")
+		}
+		defer mux.Unlock()
+		time.Sleep(time.Duration(rand.Intn(1000)) * time.Millisecond)
+	}
+
+	// Prepare the fifo
+	out, err := RunFifoNew(ctx, newHTTPClient(), &FifoFlags{
+		endpoint: endpoint,
+		output:   "json",
+	})
+	require.NoError(err)
+	respNew := &api.FifoNewResponse{}
+	require.NoError(json.Unmarshal([]byte(out), respNew))
+
+	runRandomClient := func(wg *sync.WaitGroup) {
+		defer wg.Done()
+
+		time.Sleep(time.Duration(rand.Intn(3000)) * time.Millisecond)
+
+		out, err := RunFifoTicket(ctx, newHTTPClient(), &FifoFlags{
+			endpoint: endpoint,
+			output:   "json",
+			uuid:     respNew.UUID.String(),
+		})
+		assert.NoError(err)
+		respTicket := &api.FifoTicketResponse{}
+		assert.NoError(json.Unmarshal([]byte(out), respTicket))
+
+		assert.NoError(RunFifoWait(ctx, newHTTPClient(), &FifoFlags{
+			endpoint: endpoint,
+			output:   "json",
+			uuid:     respNew.UUID.String(),
+			ticketID: respTicket.TicketID.String(),
+		}))
+
+		assertResourceExclusive()
+
+		assert.NoError(RunFifoDone(ctx, newHTTPClient(), &FifoFlags{
+			endpoint: endpoint,
+			output:   "json",
+			uuid:     respNew.UUID.String(),
+			ticketID: respTicket.TicketID.String(),
+		}))
+	}
+
+	var wg sync.WaitGroup
+	n := 100
+	wg.Add(n)
+	for i := 0; i < n; i++ {
+		go runRandomClient(&wg)
+	}
+	wg.Wait()
 }
 
 func endpoint() string {
