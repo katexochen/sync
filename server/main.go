6f7cfeb81a8bcbc84de659ae80e6abcac773b9e2
Paul Meyer <katexochen0@gmail.com>
2025-07-07 12:46:05 +0200

    server: use persistent database for fifo backend
    Signed-off-by: Paul Meyer <katexochen0@gmail.com>

--- server/main.go
+++ server/main.go
@@ -1,18 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"log/slog"
 	"net/http"
 	"os"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
 func main() {
 	log := slog.New(slog.NewTextHandler(os.Stderr, nil))
 	log.Info("started")
 
+	db, err := gorm.Open(sqlite.Open("state"), &gorm.Config{})
+	if err != nil {
+		log.Error("fatal", "err", fmt.Errorf("open sqlite: %w", err))
+		os.Exit(1)
+	}
+
 	mux := http.NewServeMux()
-	fm := newFifoManager(log)
-	fm.registerHandlers(mux, "/fifo")
+	fm := newFifoManager(db, log)
+	fm.registerHandlers(mux)
 
 	if err := http.ListenAndServe(":8080", mux); err != nil {
 		log.Error("fatal", "err", err)
